4e379347165115e72044f312b5babc976591b0b1
DoloresPGz <dolores.pgz@gmail.com>
2023-07-10 20:33:11 -0600

    Componente de Home y ajuste de diseño de App

---
+++ 02-react-context/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className='home-container' >
+      <div className='izquierdo'>
+        <h2>Lado Izquierdo</h2>
+      </div>
+      <div className='derecho'>
+        <h2>Lado Derecho</h2>
+      </div>
+    </div>
+  )
+}
+export default Home
