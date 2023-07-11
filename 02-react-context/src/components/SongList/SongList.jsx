bed71f40097ebe947060c079e15ab20f5bbd0d7c
César Guerra <warderer@gmail.com>
2023-07-10 21:26:25 -0600

    ✨ SongList Basico

--- 02-react-context/src/components/SongList/SongList.jsx
+++ 02-react-context/src/components/SongList/SongList.jsx
@@ -1,12 +1,30 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import canciones from '@/assets/listaCanciones.json'
-import 'songlist.css'
+import './songlist.css'
 
 const SongList = () => {
   const [list, setList] = useState([])
   const [loading, setLoading] = useState(true)
+
+  // Simular una llamada a la API despues de "x" tiempo
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return (
-    <div>SongList</div>
+    <section>
+      {loading
+        ? <h1>Cargando...</h1>
+        : list.map((song) => (
+          <div key={song.id} className='row-song'>
+            <h4>{song.title}</h4>
+            <p>{song.artist}</p>
+          </div>
+        ))}
+    </section>
   )
 }
 export default SongList
--- 02-react-context/src/pages/Home/Home.jsx
+++ 02-react-context/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
+// import SongList from '../../components/SongList/SongList'
+import SongList from '@/components/SongList'
 import './home.css'
 
 const Home = () => {
   return (
-    <div className='home-container' >
+    <div className='home-container'>
       <div className='izquierdo'>
         <h2>Lado Izquierdo</h2>
+        <SongList />
       </div>
       <div className='derecho'>
         <h2>Lado Derecho</h2>
