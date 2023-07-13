b9134e2a3b7646522072ea0cbce671324870813a
DoloresPGz <dolores.pgz@gmail.com>
2023-07-12 20:32:57 -0600

    SongDetail con contexto

--- 02-react-context/src/components/SongDetail/SongDetail.jsx
+++ 02-react-context/src/components/SongDetail/SongDetail.jsx
@@ -1,6 +1,18 @@
+import { useSongContext } from '@/hooks/useSongContext'
 const SongDetail = () => {
+  const {selectedSong} = useSongContext()
   return (
-    <div>SongDetail</div>
+    <>
+      {
+        selectedSong.title
+          ? <div>
+            <h2>{selectedSong.title}</h2>
+            <h4>{selectedSong.artist}</h4>
+            <h6>{selectedSong.year}</h6>
+          </div>
+          : <h1>Seleccione una canción</h1>
+      }
+    </>
   )
 }
 export default SongDetail
--- 02-react-context/src/components/SongList/SongList.jsx
+++ 02-react-context/src/components/SongList/SongList.jsx
@@ -2,13 +2,17 @@ import { useSongContext } from '@/hooks/useSongContext'
 import './songlist.css'
 
 const SongList = () => {
-  const {list, loading} = useSongContext()
+  const {list, loading, setSelectedSong} = useSongContext()
   return (
     <section>
       {loading
         ? <h1>Cargando...</h1>
         : list.map((song) => (
-          <div key={song.id} className='row-song'>
+          <div
+            key={song.id}
+            className='row-song'
+            onClick={() => setSelectedSong(song)}
+          >
             <h4>{song.title}</h4>
             <p>{song.artist}</p>
           </div>
