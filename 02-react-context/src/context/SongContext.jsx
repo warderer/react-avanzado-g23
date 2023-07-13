a663e0daacca95c7f8b94cc4db5c6075bd2fb1c1
DoloresPGz <dolores.pgz@gmail.com>
2023-07-12 19:49:43 -0600

    Creación del contexto, proveedor y consumidores

---
+++ 02-react-context/src/context/SongContext.jsx
@@ -0,0 +1,39 @@
+// CONTEXT se encagar de manejar los estados globales de React
+// Nos permite compartir la misma información entre componentes que encuentran en distintas jerarquías del árbol de react
+
+import { useState, useEffect, createContext } from 'react'
+import canciones from '@/assets/listaCanciones.json'
+
+// Paso 1. Crear el contexto
+const SongContext = createContext()
+
+// Paso 2. Crear el proveedor del contexto
+
+function SongProvider (props) {
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [selectedSong, setSelectedSong] = useState({}) // Identifica la canción seleccionada de la lista
+
+  // Simular una llamada a la API despues de "x" tiempo
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
+  const data = {
+    list,
+    loading,
+    selectedSong,
+    setSelectedSong
+  }
+
+  return (
+    <SongContext.Provider value={data} >
+      {props.children}
+    </SongContext.Provider>
+  )
+}
+
+export {SongProvider, SongContext}
--- 02-react-context/src/pages/Home/Home.jsx
+++ 02-react-context/src/pages/Home/Home.jsx
@@ -2,18 +2,21 @@
 import SongList from '@/components/SongList'
 import SongDetail from '@/components/SongDetail'
 import './home.css'
+import { SongProvider } from '@/context/SongContext'
 
 const Home = () => {
   return (
     <div className='home-container'>
-      <div className='izquierdo'>
-        <h2>Lado Izquierdo</h2>
-        <SongList />
-      </div>
-      <div className='derecho'>
-        <h2>Lado Derecho</h2>
-        <SongDetail />
-      </div>
+      <SongProvider>
+        <div className='izquierdo'>
+          <h2>Lado Izquierdo</h2>
+          <SongList />
+        </div>
+        <div className='derecho'>
+          <h2>Lado Derecho</h2>
+          <SongDetail />
+        </div>
+      </SongProvider>
     </div>
   )
 }
