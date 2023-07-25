85c5b5ccabcc562367e7d3d1cd435a89309ce65e
César Guerra <warderer@gmail.com>
2023-07-24 21:33:20 -0600

    ✨ Datos del Usuario Admin

--- 03-react-auth/src/pages/Dashboard.jsx
+++ 03-react-auth/src/pages/Dashboard.jsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from 'react'
+import { useAuthContext } from '@/hooks/useAuthContext'
+import { getSingleUserService } from '@/services/userServices'
+
 const Dashboard = () => {
+  const { userPayload } = useAuthContext()
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getSingleUserService(userPayload.id)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.error('Ocurrio un error en Dashboard', error.message)
+      }
+    }
+    fetchUserData()
+  }, [userPayload.id])
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      {
+      userData?.first_name && <h2> {userData.first_name}</h2>
+      }
+      {
+      userData?.last_name && <h2> {userData.last_name}</h2>
+      }
+      {
+      userData?.gender && <h2> {userData.gender}</h2>
+      }
+      {
+      userData?.email && <h2> {userData.email}</h2>
+      }
+    </>
   )
 }
 export default Dashboard
--- 03-react-auth/src/services/userServices.js
+++ 03-react-auth/src/services/userServices.js
@@ -2,7 +2,24 @@ import axios from 'axios'
 
 const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
+// Referencia: https://stackoverflow.com/questions/71989146/axios-instance-not-getting-the-token-from-local-storage-on-first-call-react-js
+// Si existe un token, inyectalo en la cabecera de la petición
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getSingleUserService = (id) => axios.get(`${BASE_URL}/users/${id}`)
 
-export { registerUserService, loginUserService }
+export { registerUserService, loginUserService, getSingleUserService }
