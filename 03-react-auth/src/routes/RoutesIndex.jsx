926b68efab9237cd9b9dd87cb229739c579c6bc1
César Guerra <warderer@gmail.com>
2023-07-24 21:06:14 -0600

    ✨ Ruta Dashboard Protegida

--- 03-react-auth/src/routes/RoutesIndex.jsx
+++ 03-react-auth/src/routes/RoutesIndex.jsx
@@ -1,11 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from '@/hooks/useAuthContext'
 import { Home, Dashboard, Login, Secret, Signup } from '@/pages'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route
+        path='/dashboard'
+        element={
+          isAuth
+            ? <Dashboard />
+            : <Navigate to='/login' replace />
+      }
+      />
       <Route path='/login' element={<Login />} />
       <Route path='/secret' element={<Secret />} />
       <Route path='/signup' element={<Signup />} />
